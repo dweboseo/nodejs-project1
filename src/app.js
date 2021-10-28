c7ce3cebd5fada40f4121b4df99c08d6b0966c86
Deepak <deepak@weboseo.com>
2021-10-28 13:13:04 +0000

    Port fetch from ENV variable, setup start command package.json

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define paths foe Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -43,6 +44,6 @@ app.get('*', (req, res) => {
 })
 
 //Starting the express server
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is running on port 3000')
 })
